3415ebea3717900b7f9a4d6a81ab2984d7009a05
maniek <mpastuszka@gmail.com>
2022-01-23 10:26:57 +0100

    [change] - sending query parameters to endpoint

--- src/main/java/com/sda/javaee9spring/controller/FirstController.java
+++ src/main/java/com/sda/javaee9spring/controller/FirstController.java
@@ -1,12 +1,9 @@
 package com.sda.javaee9spring.controller;
 
-import lombok.Data;
-import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @Slf4j
@@ -15,7 +12,7 @@ public class FirstController {
     // the same as @Slf4j
     // private static final Logger log = LoggerFactory.getLogger(FirstController.class);
 
-    @GetMapping("/my-first-page")
+    @GetMapping({"/my-first-page", "/"})
     public String firstPage() {
         log.info("firstPage() method called!!!");
         // looking for html page inside resources/templates
@@ -30,9 +27,24 @@ public class FirstController {
         return "second-page";
     }
 
-    @GetMapping("/third-page")
+//    @GetMapping("/third-page")
+//    @GetMapping(value = "/third-page")
+//    @GetMapping(value = {"/third-page"}, params = {})
+    @GetMapping(value = {"/third-page", "/third-too", "/abcd"})
     public String thirdPage() {
         log.info("thirdPage() method was called!!!");
         return "pages/third-page";
+    }
+
+    // /my-name?name=mariusz&surname=pastuszka
+    // name param with value mariusz
+    // surname param with value pastuszka
+    @GetMapping("/my-name")
+    public String myName(@RequestParam("name") String myFirstName, @RequestParam("surname") String mySurname) {
+        log.info("myName() method was called!!!");
+        log.info("my name is: [{}] and my surname is: [{}]", myFirstName, mySurname);
+        log.info(String.format("my name is: [%s] and my surname is: [%s]", myFirstName, mySurname));
+
+        return "";
     }
 }
