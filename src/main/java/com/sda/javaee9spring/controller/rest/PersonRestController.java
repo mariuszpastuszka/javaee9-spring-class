5077ff4a46b54482dff70e140c2771d99adf2d21
maniek <mpastuszka@gmail.com>
2022-02-13 13:05:47 +0100

    [add] - find person by id endpoint created

--- src/main/java/com/sda/javaee9spring/controller/rest/PersonRestController.java
+++ src/main/java/com/sda/javaee9spring/controller/rest/PersonRestController.java
@@ -5,6 +5,7 @@ import com.sda.javaee9spring.service.RealPersonService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,5 +28,15 @@ public class PersonRestController {
         log.info("findAllPersons()");
 
         return personService.readAllPersonEntities();
+    }
+
+    // /persons/1
+    // /persons/10
+    // /persons/12345
+    @GetMapping("/persons/{id}")
+    public PersonEntity findPersonEntityById(@PathVariable("id") Long personId) {
+        log.info("trying to find person entity by id: [{}]", personId);
+
+        return personService.readPersonEntityById(personId);
     }
 }
